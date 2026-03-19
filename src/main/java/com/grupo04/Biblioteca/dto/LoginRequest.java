79df2ac71ac26956068bbe23fe8f0362cc3d2257
FernandaTozzi <fernandatozzihonorio@gmail.com>
2026-03-19 10:52:45 -0300

    :sparkles: create LoginRequest DTO for authentication

---
+++ src/main/java/com/grupo04/Biblioteca/dto/LoginRequest.java
@@ -0,0 +1,15 @@
+package com.grupo04.Biblioteca.dto;
+
+public class LoginRequest {
+
+    private String email;
+    private String senha;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+}   
