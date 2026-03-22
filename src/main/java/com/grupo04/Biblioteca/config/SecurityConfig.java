864b994cd887259715514c4482d0c22df1d35955
Samuel Souza <silva.samueldev@gmail.com>
2026-03-22 12:22:14 -0300

    :sparkles: feat: injetar bean de decode

--- src/main/java/com/grupo04/Biblioteca/config/SecurityConfig.java
+++ src/main/java/com/grupo04/Biblioteca/config/SecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -22,6 +24,13 @@ public class SecurityConfig {
                 .oauth2ResourceServer(oauth2 ->
                         oauth2.jwt(Customizer.withDefaults()));
         return http.build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder
+                .withJwkSetUri("http://localhost:8080/realms/meu-realm/protocol/openid-connect/certs")
+                .build();
     }
 
 }
